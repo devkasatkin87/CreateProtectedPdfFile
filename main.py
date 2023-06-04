0396d93c6844018f43660a621df3faae32d6d5e4
Serhii Kasatkin <devkasatkin87@gmail.com>
2023-06-04 21:43:43 +0300

    Create encrypted pdf file with hide password

---
+++ main.py
@@ -0,0 +1,20 @@
+from PyPDF2 import PdfReader, PdfWriter
+from getpass import getpass
+
+pdf_writer = PdfWriter()
+# загружаем данные pdf через объект библиотеки
+pdf = PdfReader('sample.pdf')
+
+# получаем все страницы pdf файла и записываем их в свойство объекта
+for page in range(len(pdf.pages)):
+    pdf_writer.add_page(pdf.pages[page])
+ 
+# ввод пароля 
+password = getpass(prompt='Enter password: ')
+
+# шифруем файл с помощью пароля
+pdf_writer.encrypt(password)
+
+# записываем данные в файл
+with open('protected.pdf', 'wb') as file:
+    pdf_writer.write(file)
